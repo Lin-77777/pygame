2c61e17afa4886d2a9da8a6265851d2ac6836442
Lin-77777 <yclrx78@gmail.com>
2025-02-23 11:03:05 +0800

    feat: 新增 Pygame 繪圖範例程式碼及功能說明

--- class1/prj01.py
+++ class1/prj01.py
@@ -1,1 +1,21 @@
-
+"""
+@workspace - 提出有關您工作區的問題
+/explain - 說明使用中編輯器中的程式碼如何運作
+/tests - 為選取的程式碼產生單元測試
+/fix - 為選取的程式碼中的問題建議修正
+/new - 工作區中新檔案或專案的 Scaffold 程式代碼
+/newNotebook - 建立新的 Jupyter Notebook
+/fixTestFailure - 為失敗的測試建議修正
+/setupTests - 在專案中設定測試 (實驗性)
+@vscode - 詢問有關 VS Code 的問題
+/search - 產生工作區搜尋的查詢參數
+/startDebugging - 產生啟動設定並在 VS Code 中開始偵錯 (實驗型)
+@terminal - 詢問如何在終端機中執行某些動作
+/explain - 說明終端的內容
+@github - 從 Web 搜尋、程式碼搜尋和您企業的知識庫獲得解答基礎
+# 代表附加檔案的功能，只跟檔案有關
+#codebase:掃描專案當中所有的程式碼，並且提供有關專案的意見和建議
+#Change:掃描git變更的部分，並且提供有關變更的意見和建議
+#file:掃描單一檔案，並且提供有關檔案的意見和建議
+#Floder:掃描資料夾當中的所有檔案，並且提供有關資料夾的意見和建議
+"""
