73836c4698c37b1764e5085e88718e328f25d8c6
Lin-77777 <yclrx78@gmail.com>
2025-03-09 09:29:06 +0800

    feat: 新增簡單畫圖工具，使用者可透過滑鼠繪製藍色線條

---
+++ class1/prj03.py
@@ -0,0 +1,36 @@
+# 這段程式碼是用來建立一個簡單的畫圖工具，使用者可以在視窗中點擊並拖動滑鼠來畫出藍色的線。。
+######################匯入模組######################
+import pygame as g  # 在終端機輸入 pip install pygame -U
+import sys
+
+######################初始化######################
+g.init()  # 啟動 Pygame
+width = 1000  # 設定視窗寬度
+height = 600  # 設定視窗高度
+######################建立視窗及物件######################
+# 設定視窗大小
+screen = g.display.set_mode((width, height))
+# 設定視窗標題
+g.display.set_caption("畫圖工具")
+#####################建立畫布######################
+# 建立畫布
+bg = g.Surface((width, height))
+# 畫布為白色(R，G，B)
+bg.fill((255, 255, 255))
+a = False
+######################循環偵測######################
+while True:
+    for event in g.event.get():
+        if event.type == g.QUIT:  # 如果按下[X]按鈕就退出
+            sys.exit()  # 離開遊戲
+        if event.type == g.MOUSEBUTTONDOWN:
+            a = True
+        if a == True:
+            x, y = g.mouse.get_pos()  # 取得滑鼠座標
+            g.draw.circle(bg, (0, 0, 255), (x, y), 10, 0)
+        if event.type == g.MOUSEBUTTONUP:
+            a = False
+    screen.blit(
+        bg, (0, 0)
+    )  # 以左上角為起點，橫線為X座標，直線為Y座標，前面寫的是X座標，後面寫的是Y座標
+    g.display.update()  # 更新視窗
